fde532f2a832235924b21cb7238b6a1fbe6d26c9
Maiwand Kakar <en.maiwand@gmail.com>
2024-04-19 23:48:19 +0300

    Building Game Cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,6 @@
-import { Text } from '@chakra-ui/react';
+import { SimpleGrid, Text } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
+import GameCard from './GameCard';
 
 const GameGrid = () => {
 
@@ -8,12 +9,11 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <ul>
-          {games.map(game => (
-            <li key={game.id}>{game.name} </li>
+      <SimpleGrid columns={{sm:2, md:2, lg:3, xl:5}} spacing={10} padding={2}>
+      {games.map(game => (
+            <GameCard key={game.id} game={game}/>
           ))}
-
-      </ul>
+      </SimpleGrid>
     </>
   )
 }
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,10 @@ import { useEffect, useState } from "react"
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 
 interface FetchGamesResponse {
@@ -18,27 +18,23 @@ const useGames = () => {
     const [games, setGames] = useState<Game[]>([]) // Specify the type of the state as an array of Game objects
     const [error, setError] = useState('')
     // using useEffect to fetch data from the API
-
-
     useEffect(() =>{
       const controller = new AbortController();
-
       // Fetch the data from the API
-
       apiClient.get<FetchGamesResponse>('/games', { signal: controller.signal })
         .then((response) => setGames(response.data.results)) // Access the response data when setting the state
-
         // Catch any errors and set the error state
         .catch((error) => {
           if (error instanceof CanceledError ) return;
           setError(error.message);
         })
+      //  if (error instanceof CanceledError): This line checks if the error is specifically a CanceledError.
+      // If it is, the function returns early (i.e., no further code execution), effectively ignoring the error.
+      // If it’s a different type of error (not related to cancellation), the setError function is called with the error message.
 
         // Clean up function
         return () => controller.abort();
-
     }, []);
-
     return ({ games, error});
 }
 
