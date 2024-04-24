4b8cc5daecae788e8a2a77c8319dd01c9a2eaaba
Maiwand Kakar <en.maiwand@gmail.com>
2024-04-24 06:46:28 +0300

    Changed API keys.

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params:{
-    //  key: '4bdaf0f7281f473fb915554396194342'
-      key: import.meta.env.VITE_REACT_APP_API_KEY
+     key: '4bdaf0f7281f473fb915554396194342'
+      // key: import.meta.env.VITE_REACT_APP_API_KEY
     // key: process.env.VITE_REACT_APP_API_KEY
 
   }
